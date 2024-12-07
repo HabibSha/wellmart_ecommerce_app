c64571f3e1a3edad2721e75e5868f210104513e9
habibsha <habibshahalahi7424@gmail.com>
2024-12-07 22:12:14 +0600

    File filter with validation

--- backend/src/middleware/fileUpload.ts
+++ backend/src/middleware/fileUpload.ts
@@ -1,19 +1,44 @@
 import multer from "multer";
 import path from "path";
+import createError from "http-errors";
 
-import { uploadDirectory } from "../secret";
+const UPLOAD_DIR = process.env.UPLOAD_FILE || "public/images/users";
+const MAX_FILE_SIZE = Number(process.env.MAX_FILE_SIZE) || 2097152; // bytes = 2 mb = 1024*1024*2
+const ALLOWED_FILE_TYPES = process.env.ALLOWED_FILE_TYPES || [
+  "jpg",
+  "jpeg",
+  "png",
+];
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, uploadDirectory); // Temporary directory
+    cb(null, UPLOAD_DIR); // Temporary directory
   },
   filename: (req, file, cb) => {
     const extname = path.extname(file.originalname);
-    // const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, Date.now() + "-" + file.originalname);
+    cb(
+      null,
+      Date.now() + "-" + file.originalname.replace(extname, "") + extname
+    );
   },
 });
 
-const upload = multer({ storage });
+const fileFilter = (
+  _req: any,
+  file: { originalname: string },
+  cb: (arg0: any, arg1?: boolean | undefined) => void
+) => {
+  const extname = path.extname(file.originalname);
+  if (!ALLOWED_FILE_TYPES.includes(extname.substring(1))) {
+    return cb(createError(400, "File type not allowed"));
+  }
+  cb(null, true);
+};
+
+const upload = multer({
+  storage,
+  limits: { fileSize: MAX_FILE_SIZE },
+  fileFilter,
+});
 
 export default upload;
--- backend/src/secret.ts
+++ backend/src/secret.ts
@@ -3,6 +3,5 @@ const mongoUri =
   process.env.MongoDB_URI || "mongodb://127.0.0.1:27017/WellMartDB";
 const jwtAccessKey = process.env.JWT_ACCESS_KEY || "jwtsecretaccesskey";
 const jwtRefreshKey = process.env.JWT_REFRESH_KEY || "jwtsecretrefreshkey";
-const uploadDirectory = process.env.UPLOAD_FILE || "public/images/users";
 
-export { port, mongoUri, jwtAccessKey, jwtRefreshKey, uploadDirectory };
+export { port, mongoUri, jwtAccessKey, jwtRefreshKey };
