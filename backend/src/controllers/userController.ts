0e5fc4f3cbef0ac45b62da42b7b796ba9398a177
habibsha <habibshahalahi7424@gmail.com>
2024-12-09 22:52:41 +0600

    Update user register controller for default image path

--- backend/src/controllers/userController.ts
+++ backend/src/controllers/userController.ts
@@ -5,6 +5,7 @@ import fs from "fs";
 import User from "../models/userModel";
 import { successResponse } from "./responseController";
 import uploadToCloudinary from "../helper/uploadToCloudinary";
+import { defaultImagePath } from "../secret";
 
 // Create a new user - Process register
 const handleUserRegister = async (
@@ -23,24 +24,28 @@ const handleUserRegister = async (
       );
     }
 
-    const file = req.file;
+    let imageUrl: string = defaultImagePath;
 
-    if (!file) {
-      throw createError(400, "No file uploaded");
-    }
-    if (file.size > 1024 * 1024 * 2) {
-      throw createError(400, "File too large. It must be less than 2 MB");
-    }
+    if (req.file) {
+      const file = req.file;
 
-    const image = file?.path;
-    const result = await uploadToCloudinary(image);
-    if (!result || !result.secure_url) {
-      throw new Error("Failed to upload image to Cloudinary");
-    }
-    const imageUrl = result.secure_url;
+      if (file.size > 1024 * 1024 * 2) {
+        throw createError(400, "File too large. It must be less than 2 MB");
+      }
 
-    // Optionally delete the file from the local server
-    fs.unlinkSync(image);
+      const image = file?.path;
+
+      // Upload the file to Cloudinary
+      const result = await uploadToCloudinary(image);
+      if (!result || !result.secure_url) {
+        throw new Error("Failed to upload image to Cloudinary");
+      }
+
+      imageUrl = result.secure_url;
+
+      // Optionally delete the file from the local server
+      fs.unlinkSync(image);
+    }
 
     const newUser = { name, email, password, image: imageUrl };
     const user = await User.create(newUser);
