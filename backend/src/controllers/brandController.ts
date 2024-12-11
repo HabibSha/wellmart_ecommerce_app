098e43469cba29ffb9cdeb67a80476f40015f20c
habibsha <habibshahalahi7424@gmail.com>
2024-12-11 22:03:16 +0600

    Create brand controller

---
+++ backend/src/controllers/brandController.ts
@@ -0,0 +1,35 @@
+import { Request, Response, NextFunction } from "express";
+import slugify from "slugify";
+import createError from "http-errors";
+
+import Brand from "../models/brandModel";
+import { successResponse } from "./responseController";
+
+const handleBrandCreate = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+): Promise<void> => {
+  const { name } = req.body;
+  try {
+    const existingBrand = await Brand.findOne();
+    if (existingBrand) {
+      throw createError(400, "Brand name is already exists");
+    }
+
+    const newBrand = {
+      name: name,
+      slug: slugify(name),
+    };
+
+    const brand = await Brand.create(newBrand);
+
+    successResponse(res, {
+      statusCode: 200,
+      message: "Brand was created successfully",
+      payload: brand,
+    });
+  } catch (error) {
+    next(error);
+  }
+};
