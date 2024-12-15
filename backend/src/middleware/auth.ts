166d3f913e3109e171e5a1cea30cb3641504f57f
habibsha <habibshahalahi7424@gmail.com>
2024-12-15 21:39:20 +0600

    Create a auth middleware and make a isLoggedIn middleware in it

---
+++ backend/src/middleware/auth.ts
@@ -0,0 +1,26 @@
+import { Request, Response, NextFunction } from "express";
+import createError from "http-errors";
+import jwt from "jsonwebtoken";
+
+import { jwtAccessKey } from "../secret";
+
+const isLoggedIn = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+    if (!accessToken) {
+      throw createError(401, "Access token not found. Please login");
+    }
+
+    const decoded = jwt.verify(accessToken, jwtAccessKey);
+    if (!decoded) {
+      throw createError(401, "Invalid access token. Please login");
+    }
+
+    // req.user = decoded.user;
+    next();
+  } catch (error) {
+    return next(error);
+  }
+};
+
+export { isLoggedIn };
