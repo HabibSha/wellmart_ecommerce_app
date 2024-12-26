d1abf93f665b9ac33ca9926b0383d63cc9c6fe8b
habibsha <habibshahalahi7424@gmail.com>
2024-12-26 17:50:25 +0600

    Updates brand controller

--- backend/src/controllers/brandController.ts
+++ backend/src/controllers/brandController.ts
@@ -4,6 +4,8 @@ import createError from "http-errors";
 
 import Brand from "../models/brandModel";
 import { successResponse } from "./responseController";
+import uploadToCloudinary from "../helper/uploadToCloudinary";
+import unlinkImageFromLocal from "../helper/unlinkImageFromLocal";
 
 // Create a brand
 const handleCreateBrand = async (
@@ -12,15 +14,36 @@ const handleCreateBrand = async (
   next: NextFunction
 ): Promise<void> => {
   const { title } = req.body;
+
+  let uploadedImageResult = null;
+  let imageUrl = "";
+
   try {
     const existingBrand = await Brand.exists({ title });
     if (existingBrand) {
       throw createError(400, "Brand title is already exists");
     }
 
+    // handle upload image
+    const image = req.file;
+    if (!image) {
+      throw createError(400, "Brand image is required");
+    }
+
+    if (image.size > 1024 * 1024 * 2) {
+      throw createError(400, "File is too large. It must be less than 2 MB");
+    }
+
+    uploadedImageResult = await uploadToCloudinary(image.path);
+    imageUrl = uploadedImageResult.secure_url;
+
+    // Delete image from local file storage
+    unlinkImageFromLocal(image.path);
+
     const newBrand = {
       title: title,
       slug: slugify(title, { lower: true }),
+      image: imageUrl,
     };
 
     const brand = await Brand.create(newBrand);
