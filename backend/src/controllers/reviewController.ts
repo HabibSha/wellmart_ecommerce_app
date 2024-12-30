697558452a238844505fba066a6ccfdeb7b35636
habibsha <habibshahalahi7424@gmail.com>
2024-12-30 19:09:07 +0600

    Create review handler

--- backend/src/controllers/reviewController.ts
+++ backend/src/controllers/reviewController.ts
@@ -1,13 +1,31 @@
 import { Request, Response, NextFunction } from "express";
 
+import Review from "../models/reviewModel";
+import { successResponse } from "./responseController";
+
 const handleCreateReview = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  const { slug } = req.params;
   const { rating, message } = req.body;
 
   try {
+    const newReview = {
+      rating,
+      message,
+      user: req.userId,
+      product: slug,
+    };
+
+    const review = await Review.create(newReview);
+
+    successResponse(res, {
+      statusCode: 201,
+      message: "Review has been successfully created",
+      payload: review,
+    });
   } catch (error) {
     next(error);
   }
