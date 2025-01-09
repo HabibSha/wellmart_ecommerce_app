55c76b3cb46b4f9564f90ad0a40a97a7d99b856b
habibsha <habibshahalahi7424@gmail.com>
2025-01-09 20:56:52 +0600

    Config handle get route

--- backend/src/routers/cartRouter.ts
+++ backend/src/routers/cartRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 
-import { handleAddToCart } from "../controllers/cartController";
+import { handleAddToCart, handleGetCart } from "../controllers/cartController";
 import { isLoggedIn } from "../middleware/auth";
 
 const cartRouter = Router();
 
 cartRouter.post("/:productId", isLoggedIn, handleAddToCart);
+cartRouter.get("/:productId", isLoggedIn, handleGetCart);
 
 export default cartRouter;
