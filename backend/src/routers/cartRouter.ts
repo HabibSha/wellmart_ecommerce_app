9dc7679ab74ffc1dd8ccff23ae8cde00eb9fb87d
habibsha <habibshahalahi7424@gmail.com>
2025-01-09 20:59:20 +0600

    Config handle remove cart

--- backend/src/routers/cartRouter.ts
+++ backend/src/routers/cartRouter.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 
-import { handleAddToCart, handleGetCart } from "../controllers/cartController";
+import {
+  handleAddToCart,
+  handleGetCart,
+  handleRemoveCart,
+} from "../controllers/cartController";
 import { isLoggedIn } from "../middleware/auth";
 
 const cartRouter = Router();
 
 cartRouter.post("/:productId", isLoggedIn, handleAddToCart);
 cartRouter.get("/:productId", isLoggedIn, handleGetCart);
+cartRouter.delete("/", isLoggedIn, handleRemoveCart);
 
 export default cartRouter;
