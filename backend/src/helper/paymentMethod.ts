ae140ddaea5df16b278b5987c468067c0d32624e
habibsha <habibshahalahi7424@gmail.com>
2025-01-13 21:12:33 +0600

    Configure payment integration

--- backend/src/helper/paymentMethod.ts
+++ backend/src/helper/paymentMethod.ts
@@ -2,9 +2,27 @@ import stripe from "stripe";
 
 import stripeSecret from "../secret";
 
-const paymentMethod = async () => {
+interface IPayment {}
+
+const stripePayment = async (number, cvc, expMonth, expYear, amount) => {
   try {
+    const paymentMethod = await stripe.paymentMethods.create({
+      type: "card",
+      card: {
+        number: number,
+        exp_month: expMonth,
+        exp_year: expYear,
+        cvc: cvc,
+      },
+    });
+
+    const paymentIntents = await stripe.paymentIntents.create({
+      amount: amount * 100,
+      currency: "BDT",
+      payment_method: paymentMethod,
+      confirm: true,
+    });
   } catch (error) {}
 };
 
-export default paymentMethod;
+export default stripePayment;
