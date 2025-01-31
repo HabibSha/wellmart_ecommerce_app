e0b4bfaac9e7ec20161b1716b6c25316e52f49b4
habibsha <habibshahalahi7424@gmail.com>
2025-01-31 21:53:22 +0600

    Config authorization in category router

--- backend/src/routers/categoryRouter.ts
+++ backend/src/routers/categoryRouter.ts
@@ -10,6 +10,7 @@ import {
 import { uploadCategoryImage } from "../middleware/fileUpload";
 import validateCategory from "../validation/category";
 import runValidation from "../validation";
+import { isLoggedIn } from "../middleware/auth";
 
 const categoryRouter = Router();
 
@@ -20,9 +21,9 @@ categoryRouter.post(
   runValidation,
   handleCreateCategory
 );
-categoryRouter.get("/", handleGetCategories);
-categoryRouter.get("/:slug", handleGetCategory);
-categoryRouter.delete("/:slug", handleDeleteCategory);
-categoryRouter.put("/:slug", handleUpdateCategory);
+categoryRouter.get("/", isLoggedIn, handleGetCategories);
+categoryRouter.get("/:slug", isLoggedIn, handleGetCategory);
+categoryRouter.delete("/:slug", isLoggedIn, handleDeleteCategory);
+categoryRouter.put("/:slug", isLoggedIn, handleUpdateCategory);
 
 export default categoryRouter;
